91fe112f731f2ab4834b91537abd0d06bbc74454
TimotheGonin <gonin.timothe@gmail.com>
2021-08-28 11:20:53 +0200

    fix : lauch nav bar

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,17 +1,21 @@
-// function editNav() {
-// 	var x = document.getElementById("myTopnav");
-// 	if (x.className === "topnav") {
-// 		x.className += " responsive";
-// 	} else {
-// 		x.className = "topnav";
-// 	}
-// }
+function editNav() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
 
 // DOM Elements
+const navBarButton = document.getElementById("navBarButton");
 const modalbg = document.querySelector(".bground"); // modal
 const modalBtn = document.querySelectorAll(".modal-btn"); //button active modal
 const modalBtnClose = document.querySelectorAll(".bground .close"); //button close modal
 // const formData = document.querySelectorAll(".formData"); // form into modal
+
+//launch navbar event
+navBarButton.addEventListener("click", editNav);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
